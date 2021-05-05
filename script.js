60041c016c7816851be2a6cbd05d939ab4cc9806
chriskoder <gerokristof0909@gmail.com>
2021-05-05 22:09:51 +0200

    Elso commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const kifogas = document.querySelector('.kifogas')
+const generaloGomb = document.querySelector('.btn-generate')
+
+const kifogasok = [
+    'Nem tudtam digitális oktatásban tanulni',
+    'A tanárom nem tartott órát',
+    'Túl nehéz volt a feladatsor',
+    'Hazudtak a szivárogtatott témáról :(',
+    'Szakközépbe járok',
+    'Nem ugyanolyan volt, mint tavalyi',
+    'Be voltam zárva és mély depresszióba estem :(',
+    'Szar a magyar oktatás',
+    'Az lg-t 1g-nek néztem, szar volt a betűtípus :(',
+    'Én egyszerűen nem értem a matekot :(',
+    'Kevés volt az idő a felkészülésre',
+    'Az előző évi sokkal könnyebb volt',
+    'A TAVALYI MATEKON CSAK SZÍNEZNI KELLETT MOST MEG GONDOLKODNI IS :(',
+    'AZ ÍRÓ NYITOTT VÉGET HAGYOTT A NOVELLÁBAN :(',
+    'Nekem egyszerűen nincs érzékem a nyelvekhez :(',
+    'Nem rakták fel előtte a megoldókulcsot',
+    'A próbaérettségi sokkal jobban ment',
+    'Elavult a magyar oktatási rendszer',
+    'Magyarországon nincs megfelelő nyelvoktatás',
+    'Rosszul volt megfogalmazva a feladat',
+    'Nem az volt a töriben amit megtanultam'
+]
+
+let generaltKifogas = ""
+generaloGomb.addEventListener("click", () => {
+    generaltKifogas = kifogasok[Math.floor(Math.random() * kifogasok.length)];
+    kifogas.textContent = generaltKifogas
+})
+
